5239c2afff0fcce76fb2e60f93eca9cd6ea13fe1
CH <hbac11@naver.com>
2022-12-14 02:10:38 +0900

    day12.14

---
+++ web_workspace3/day3/src/main/java/day3/chatting/ClientTest.java
@@ -0,0 +1,10 @@
+package day3.chatting;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ web_workspace3/day3/src/main/java/day3/chatting/ServerTest.java
@@ -0,0 +1,12 @@
+package day3.chatting;
+
+public class ServerTest {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

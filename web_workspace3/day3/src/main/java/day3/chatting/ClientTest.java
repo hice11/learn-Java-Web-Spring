1b3fb54b49b5fee16db58f518cb406feb7d70d07
ch <hbac11@naver.com>
2022-12-14 17:51:07 +0900

    daty12.14

--- web_workspace3/day3/src/main/java/day3/chatting/ClientTest.java
+++ web_workspace3/day3/src/main/java/day3/chatting/ClientTest.java
@@ -1,6 +1,34 @@
 package day3.chatting;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ClientTest {
+	
+	Socket socket;
+	
+	DataInputStream dataInputStream;
+	DataOutputStream dataOutputStream;
+	
+	public ClientTest() {
+		try {
+			socket = new Socket("localhost",6000);
+			System.out.println("클라이언트 접속");
+			
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- web_workspace3/day3/src/main/java/day3/chatting/ServerTest.java
+++ web_workspace3/day3/src/main/java/day3/chatting/ServerTest.java
@@ -1,12 +1,81 @@
 package day3.chatting;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ServerTest {
 	
+	ServerSocket serverSocket;
+	Socket socket;
 	
+	DataInputStream dataInputStream;
+	DataOutputStream dataOutputStream;
+	
+	public ServerTest() {
+		// TODO Auto-generated constructor stub
+		try {
+			serverSocket = new ServerSocket(6000);
+			System.out.println("서버소켓 연결");
+			socket = serverSocket.accept();
+			System.out.println("클라이언트소켓 연결");
+			
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			
+			dataRecv();
+			dataSend();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void dataSend() {
+		Scanner sc = new Scanner(System.in);
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					while(true) {
+						String message = sc.nextLine();
+						dataOutputStream.writeUTF(message);
+						
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+	}
+
+	private void dataRecv() {
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					while(true) {
+						String message = dataInputStream.readUTF();
+						System.out.println(message);
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new ServerTest();
 	}
 
 }
